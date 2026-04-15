4ac977fb926e34d6680449ec3af87ba5d91bea3a
Imran <imranbd924@gmail.com>
2026-04-15 18:54:44 +0600

    third commit

---
+++ script-4.js
@@ -0,0 +1,194 @@
+let name = "md Imran khan";
+let color = "black";
+const eggPrice = 20;
+const onionPrice = 10;
+
+const priceP = 11;
+const onionP = 3;
+const sugar = '20';
+const daddy = '20';
+
+
+
+// //const price = 35;
+// const inboxEAsenPrice = price + 10;
+// const newPrice = price + 50;
+// const discountedPrice = newPrice - 10;
+
+
+
+const income = 100;
+const cost = 40;
+
+
+// tor kase 10 ta pansil ase. portibar tui akshate 2 ta pancil babohar korbi.
+//  koto bar babohar korte parbe? program likhe ber kor.;
+
+const pancil = 10;
+const use = 2;
+
+//tor kase 60 taka ase. tui jodi portidin 15 taka kharoch koris, koybar kharoch
+//  korte parbe ? and kharoch ar por koy taka baki thakbe, seta ber korte akhata
+//  program likh.;
+
+const taka = 60;
+const costt = 15;
+
+
+//dor tui 2 ta sting likhli "hello" ar "World". ei 2 ta string ke ekh shate jora dia kibabe
+//"HelloWorld"banano jar, sete dekhanor jonno akta program likkhen.;
+
+const hello = "Hello";
+const world = "World";
+
+
+//tui jodi 153 kejin chal ar 261 keji dhal kinli ,tahole tui koy keji jinis kinesis ?
+//mot keji ber kor .;
+
+const dale = 261;
+const rice = 153;
+
+
+//tor kase 500 taka ache , portidin tui 75 taka kharoch korbi . koy bar kharoj korte parbe
+//ar seshe koy taka baki thakbe, seta ber korbar akta program likh.;
+
+const takaa = 500;
+const costtt = 75;
+
+//tui 8 k 3 dia bhag dile bagfal koto ar bagses koto , seta ber korbarjonno akhta program
+//likh.;
+
+
+const bagfal = 8;
+const bagfall = 3;
+
+const bagsesh = 8;
+const bagseshh = 3;
+
+
+
+
+// dhor akhdin gun theke uthe khida jonno chotokhato akhta rakkhosh nhoia geli .
+//  tarpor theke portidin 4 keji chaler vat khei a felis. akhon jodi toke 12 mon (480 keji)
+//chal dewa hoia thake, tahole ay chal dia tor koto din jabe. abar akh mash jodi 30 din
+//a hoy .tahole ay 12 mon chal dia tui koy din khete parbe.;
+
+
+const chal = 480
+const keji = 4;
+
+
+
+// console.log(name);
+// console.log(color);
+// console.log(eggPrice + onionPrice);
+// console.log(eggPrice - onionPrice);
+// console.log(eggPrice * onionPrice);
+// console.log(eggPrice / onionPrice);
+// console.log(priceP / onionP);
+// console.log(11 % 7);
+// console.log(sugar + daddy);
+// console.log(discountedPrice);
+// console.log(income - cost);
+// console.log(pancil / use);
+// console.log(taka / costt);
+// console.log(hello + world);
+// console.log(dale + rice);
+// console.log(takaa / costtt)
+// console.log(bagfal / bagfall)
+
+// console.log(bagsesh % bagseshh)
+// console.log(chal / keji + ' din khaite parbe')
+
+
+//Data type
+
+let passed = false;
+
+let country = "Bangladesh";
+const priceOfrice = 555;
+
+
+// "tyoeof " babohar kore program likhe dekh , kono bariabol man ki doroner . dhor akta beriabol "price" diclair
+//kor, jar man 555, akhon consol-a ata type chek kor.
+let price2 = 555;
+let learning = true;
+let megaCity = "Dhaka";
+let temperature = 102.5;
+let isHungry = true;
+let isRice = true;
+let bd = true;
+let id = "imran"
+
+
+
+// console.log(typeof passed);
+// console.log(typeof country);
+// console.log(typeof priceOfrice);
+// console.log(typeof price2);
+// console.log(typeof true);
+// console.log(typeof megaCity);
+// console.log(typeof temperature);
+// console.log(typeof isHungry);
+// console.log(typeof isRice);
+// console.log(typeof bd);
+// console.log(typeof id);
+
+// let pricee = 35;
+// pricee = 45;
+// let studyTime = 4;
+
+
+// let friendCount = 3;
+// friendCount = 2;
+
+// let phonePrice = 25000;
+// phonePrice = 12000;
+
+// let totalChairs = 40;
+// let bangaCher = 2;
+
+
+// let chairsCount = totalChairs - bangaCher;
+// let seniorBhaiChair = 6;
+// let restOfChairs = chairsCount - seniorBhaiChair;
+
+
+
+
+// console.log(pricee);
+// console.log(studyTime);
+// console.log(friendCount);
+// console.log(phonePrice);
+// console.log(chairsCount);
+// console.log(restOfChairs);
+
+// let totalChair = 40;
+// let bangaChairs = 2;
+// let chairsCount = totalChair - bangaChairs;
+// let seniorChairs = chairsCount - 6;
+
+
+// let playTime = 3;
+// playTime = 0;
+
+// //make user admin
+// const userLevel = 4;
+
+
+
+
+
+
+// console.log(chairsCount);
+// console.log(seniorChairs);
+// console.log(playTime);
+// console.log(userLevel);
+
+
+//1 typof babohar kore program likhe dekh ,kono bariabale man ki doroner dor akhta beriabal price diclar kor 
+// jar man 555 ekhon consol a ay type chek kar
+let priced = 555;
+
+console.log(priced);
+
