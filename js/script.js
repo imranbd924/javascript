9a64ede34806e96822bc126a7b6f2611b9fe27ed
MD IMRAN KHAN <imranbd924@gmail.com>
2026-04-20 15:08:15 +0600

    eight commit

--- js/script.js
+++ js/script.js
@@ -28,3 +28,41 @@ console.log(number)*/
 var number = parseFloat(prompt("Enter 1st number"))
 var number2 = parseFloat(prompt("Enter 2nd number"))
 console.log(number / number2)
+// 4 ..tui 100 taka jamanor target kors akhon porjontoh 95 taka jamaisis akta program likh chek kor tui target a powsasis ki na tar chaite beshi jomai  sis ki na.
+
+// let jamanor_target = 100;
+// let akhon_porjontoh_jamaisis = 95;
+
+// if(jamanor_target == akhon_porjontoh_jamaisis ){
+//     document.getElementById("imran").innerHTML = 'ami terget a powsaisi';
+// }else{
+//     document.getElementById("imran").innerHTML = 'ami terget a powsaini';
+// }
+
+
+//5.akta program  likhe dekh 10 or 7 ki saman  (defferent)
+
+// let number_1 = 115;
+// let number_2 = 115;
+
+// if(number_1 == number_2){
+//    document.getElementById('imran').innerHTML = number_1 + ' and ' + number_2 + '  saman';
+// }else{
+//     document.getElementById('imran').innerHTML = number_1 + ' and ' + number_2 + ' saman na ' ;
+// }
+
+// //6. 20 ki 15 er chaite kom ba saman ki na set kor
+
+let number_one  = 20;
+let number_two  = 15;
+
+if( number_one > number_two ){
+    document.getElementById("imran").innerHTML = number_one + ' baro ' + number_two + ' theke ' ;
+}
+else{
+    document.getElementById("imran").innerHTML = number_one + ' choto ' + number_two + ' theke ' ;
+}
+
+
+    </script>
+
